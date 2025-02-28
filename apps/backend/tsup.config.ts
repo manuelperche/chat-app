5e1fafa6cce6624595f45dcf3ad70487ca2e5f02
manuelperche <manuelperche@gmail.com>
2025-02-28 17:15:15 -0400

    fix: fixing vercel deploy

--- apps/backend/tsup.config.ts
+++ apps/backend/tsup.config.ts
@@ -3,6 +3,6 @@ import { defineConfig, type Options } from "tsup";
 export default defineConfig((options: Options) => ({
   entryPoints: ["api/index.ts"],
   clean: true,
-  format: ["cjs"],
+  format: ["esm"],
   ...options,
 }));
